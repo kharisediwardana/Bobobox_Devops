7db2b7f78ca4f30c557f80b0084346b5d9a4d088
kharis <kharis@mggsoftware.com>
2026-03-18 12:32:42 +0800

    add firewall

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -6,7 +6,30 @@ resource "aws_instance" "my_app" {
   ami           = "ami-0df7a207adb9748c7"
   instance_type = "t2.micro"
 
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
   tags = {
     Name = "next.sj app"
   }
+}
+
+resource "aws_security_group" "app_sg" {
+  provider = aws
+
+  # Allow HTTP (web)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow SSH (remote access)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
