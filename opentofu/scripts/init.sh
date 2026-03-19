05d349e91c1f07b50c41212f7927a87a821aba4d
kharis <kharis@mggsoftware.com>
2026-03-19 11:51:09 +0800

    move reverse proxy to workflow job

--- opentofu/scripts/init.sh
+++ opentofu/scripts/init.sh
@@ -8,22 +8,7 @@ systemctl enable docker
 
 apt install -y nginx
 
-# reverse proxy to forward port 80 → 3000
-cat > /etc/nginx/sites-available/default <<'EOF'
-server {
-    listen 80;
-    server_name _;
+echo "Hello, OpenTofu!" > /var/www/html/index.html
 
-    location / {
-        proxy_pass http://localhost:3000;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
-    }
-}
-EOF
-
-systemctl restart nginx
+systemctl start nginx
 systemctl enable nginx
