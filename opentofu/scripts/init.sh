e4987b9e3b73138344c1a0c41f7a62efad53985f
kharis <kharis@mggsoftware.com>
2026-03-19 09:34:43 +0800

    add scripts to install docker on ec2 and reverse proxy from 3000 to 80 port

--- opentofu/scripts/init.sh
+++ opentofu/scripts/init.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 apt update -y
+
+apt install -y docker.io docker-compose-v2
+systemctl start docker
+systemctl enable docker
+
 apt install -y nginx
-systemctl start nginx
+
+# reverse proxy to forward port 80 → 3000
+cat > /etc/nginx/sites-available/default <<'EOF'
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+EOF
+
+systemctl restart nginx
 systemctl enable nginx
-echo "Hello, OpenTofu!" > /var/www/html/index.html
